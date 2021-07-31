f4f647c2bb386c522a6780aa4284e6692f548528
Sagar <saggypatel93@gmail.com>
2021-07-31 12:06:33 -0700

    adding comments

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,8 @@ import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
 setCompodocJson(docJson);
 
+// Make sure you add path to your global style after loader!.
+// You can also create one style inside .storybook and import all global style in it and import that style here
 import "!style-loader!css-loader!sass-loader!../projects/my-lib/src/styles/globals.scss";
 
 export const parameters = {
